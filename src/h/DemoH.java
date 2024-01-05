f161bd7dd4dd4be25290bfe03ad713d5a88ec22c
Samuel <Samuel@192.168.0.21>
2024-01-05 15:37:19 -0800

    initial commit

---
+++ src/h/DemoH.java
@@ -0,0 +1,16 @@
+package h;
+
+public class DemoH {
+	public static void main(String[] args) {
+		System.out.println("git hub home worki");
+		
+		
+		System.out.println("ali was in here ");
+		System.out.println("2024 is here  ");
+
+		System.out.println("SAG was the best dude  ");
+		
+		System.out.println("lava di maria di santos ");
+	}
+
+}
